0225dec3b044de434177b5190d00d7578cd013b8
zehan12 <zehan9211@gmail.com>
2024-06-19 10:49:49 +0530

    remove fixture and ensure fixtures won't get generated

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,9 @@ module Granite
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.test_framework :test_unit, fixture: false
+    end
   end
 end
